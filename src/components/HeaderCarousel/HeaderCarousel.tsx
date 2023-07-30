f0814fa3b5378372daef83793b2139bad72841cb
SoftiumCode <ml2.0@mail.ru>
2023-07-30 23:19:25 +0700

    Added Header Carousel

---
+++ src/components/HeaderCarousel/HeaderCarousel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HeaderCarousel.scss';
+
+const HeaderCarousel = () => {
+  return (
+    <div className='header_carousel'>
+        <p>THE ADVENTURER</p>
+    </div>
+  )
+}
+
+export default HeaderCarousel
