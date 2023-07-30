5d995c31d8b1e063ec99938909ac68f9631e114c
SoftiumCode <ml2.0@mail.ru>
2023-07-31 00:51:04 +0700

    Some changes in footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+        <p>(c) SoftiumCode</p>
+    </footer>
+  )
+}
+
+export default Footer
